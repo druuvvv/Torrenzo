3ce76ffbd80a39b4ba0976f8af553ca804331dc0
druuvvv <104658771+druuvvv@users.noreply.github.com>
2022-12-27 16:25:18 +0530

    fixed

--- torrenzo.py
+++ torrenzo.py
@@ -1,16 +1,18 @@
 import discord
-import time
-import webbrowser
-import os
 from discord.ext import commands
+import os
 import web_scraper
 import requests
 from y1 import youtubedownloader
 
-token = <your_token>         
+Intents = discord.Intents.all()
+Intents.members = True
+Intents.message_content = True
+
+token = 'OTg5MDgyuseyourtokenmoforQgYRaNCNAxocO23Wyp7AumeI'         
 dictionary = {'link' : [0,0,0,0,0] , 'name' : [0,0,0,0,0] , 'seeder' : [0,0,0,0,0] , 'size' : [0,0,0,0,0]}
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix = '.' , intents = Intents)
 
 @client.event
 
@@ -19,18 +21,17 @@ async def on_ready():
     await client.change_presence(activity = discord.Game("with torrents \U0001F609")) 
 
 @client.command()
-async def test(ctx):
-    await ctx.send('Hello!')
+async def hello(ctx):
+  await ctx.send("wassup")
 
 @client.event
 
-async def on_message(text):
-        if text.content.startswith('/movie'):
-          torrent = text.content
+async def on_message(message):
+        if message.content.startswith('/movie'):
+          torrent = message.content
           requested_torrent ='&Category=2030&Category=2040&Category=2045&Query=' + torrent.lstrip("/movie ")
           torrent_request = requested_torrent.replace(' ', '+')
           answer = web_scraper.torrent(torrent_request)
-
 
           for i in range(5):
            try:
@@ -39,29 +40,30 @@ async def on_message(text):
             dictionary['name'][i] =name
             dictionary['link'][i] = answer['LINK'][i]
             dictionary['size'][i] = answer['Size'][i]
+            await message.channel.send(name)
             embedio = discord.Embed(
             title = answer['Title'][i] ,
-            desciption = "Embeded message" ,
-            colour = discord.Colour.blue())
-            embedio.set_author(name = text.author.name , icon_url = text.author.avatar_url)
-            embedio.add_field(name = 'Seeders' , value = answer['Seeders'][i])
-            seeders = answer['Seeders'][i]
+            description = "",
+            colour = discord.Colour.random())
+            embedio.set_author(name = message.author.name)
+            embedio.add_field(name = 'Seeders' , value = answer['Seeders'][i]) 
             size = int(answer['Size'][i])/1073741824
-            embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))           
-            await text.channel.send(embed=embedio)
+            embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))  
+                
+            await message.channel.send(embed=embedio)
             if(i==4):
-             await text.channel.send('React with these emojis 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ to indicate your choice')
+             await message.channel.send('React with these emojis 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ to indicate your choice')
            except:
             if i==0:
-              await text.channel.send("Failed to find anything.... Maybe refine the name..?")
+              await message.channel.send("Failed to find anything.... Maybe refine the name..?")
               break
             else:
-             await text.channel.send("I failed to find any further results react with  1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ if you like any result.")
+             await message.channel.send("I failed to find any further results react with  1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ if you like any result.")
              break
 
 
-        elif text.content.startswith('/tv'):
-          torrent = text.content
+        elif message.content.startswith('/tv'):
+          torrent = message.content
           requested_torrent ='&Category=5030&Category=5040&Query=' + torrent.lstrip("/tv ")
           torrent_request = requested_torrent.replace(' ', '+')
           answer = web_scraper.torrent(torrent_request)
@@ -76,27 +78,26 @@ async def on_message(text):
             dictionary['size'][i] = answer['Size'][i]
             embedio = discord.Embed(
             title = answer['Title'][i] ,
-            desciption = "Embeded message" ,
-            colour = discord.Colour.blue())
-            embedio.set_author(name = 'druvvv' , icon_url = text.author.avatar_url)
-            embedio.add_field(name = 'Seeders' , value = answer['Seeders'][i])
-            seeders = answer['Seeders'][i]
+            description = "",
+            colour = discord.Colour.random())
+            embedio.set_author(name = message.author.name)
+            embedio.add_field(name = 'Seeders' , value = answer['Seeders'][i]) 
             size = int(answer['Size'][i])/1073741824
-            embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))           
-            await text.channel.send(embed=embedio)
+            embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))            
+            await message.channel.send(embed=embedio)
             if(i==4):
-             await text.channel.send('React with these emojis 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ to indicate your choice')
+             await message.channel.send('React with these emojis 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ to indicate your choice')
            except:
             if i==0:
-              await text.channel.send("Failed to find anything.... Maybe refine the name..?")
+              await message.channel.send("Failed to find anything.... Maybe refine the name..?")
               break
             else:
-             await text.channel.send("I failed to find any further results react with  1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ if you like any result.")
+             await message.channel.send("I failed to find any further results react with  1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ if you like any result.")
              break
 
 
-        elif text.content.startswith('/anime'):
-          torrent = text.content
+        elif message.content.startswith('/anime'):
+          torrent = message.content
           requested_torrent ='&Category=100078&Category=100080&Category=100028&Query=' + torrent.lstrip("/anime ")
           torrent_request = requested_torrent.replace(' ', '+')
           answer = web_scraper.torrent(torrent_request)
@@ -112,27 +113,26 @@ async def on_message(text):
             dictionary['size'][i] = answer['Size'][i]
             embedio = discord.Embed(
             title = answer['Title'][i] ,
-            desciption = "Embeded message" ,
-            colour = discord.Colour.blue())
-            embedio.set_author(name = 'druvvv' , icon_url = text.author.avatar_url)
-            embedio.add_field(name = 'Seeders' , value = answer['Seeders'][i])
-            seeders = answer['Seeders'][i]
+            description = "",
+            colour = discord.Colour.random())
+            embedio.set_author(name = message.author.name)
+            embedio.add_field(name = 'Seeders' , value = answer['Seeders'][i]) 
             size = int(answer['Size'][i])/1073741824
-            embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))           
-            await text.channel.send(embed=embedio)
+            embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))            
+            await message.channel.send(embed=embedio)
             if(i==4):
-             await text.channel.send('React with these emojis 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ to indicate your choice')
+             await message.channel.send('React with these emojis 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ to indicate your choice')
            except:
             if i==0:
-              await text.channel.send("Failed to find anything.... Maybe refine the name..?")
+              await message.channel.send("Failed to find anything.... Maybe refine the name..?")
               break
             else:
-             await text.channel.send("I failed to find any further results react with  1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ if you like any result.")
+             await message.channel.send("I failed to find any further results react with  1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ if you like any result.")
              break
 
 
-        elif text.content.startswith('/game'):
-          torrent = text.content
+        elif message.content.startswith('/game'):
+          torrent = message.content
           requested_torrent ='&Category=4050&Query=' + torrent.lstrip("/game ")
           torrent_request = requested_torrent.replace(' ', '+')
           answer = web_scraper.torrent(torrent_request)       
@@ -147,39 +147,39 @@ async def on_message(text):
             dictionary['size'][i] = answer['Size'][i]
             embedio = discord.Embed(
             title = answer['Title'][i] ,
-            desciption = "Embeded message" ,
-            colour = discord.Colour.blue())
-            embedio.set_author(name = 'druvvv' , icon_url = text.author.avatar_url)
-            embedio.add_field(name = 'Seeders' , value = answer['Seeders'][i])
-            seeders = answer['Seeders'][i]
+            description = "",
+            colour = discord.Colour.random())
+            embedio.set_author(name = message.author.name)
+            embedio.add_field(name = 'Seeders' , value = answer['Seeders'][i]) 
             size = int(answer['Size'][i])/1073741824
-            embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))           
-            await text.channel.send(embed=embedio)
+            embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))            
+            await message.channel.send(embed=embedio)
             if(i==4):
-             await text.channel.send('React with these emojis 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ to indicate your choice')
+             await message.channel.send('React with these emojis 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ to indicate your choice')
            except:
             if i==0:
-              await text.channel.send("Failed to find anything.... Maybe refine the name..?")
+              await message.channel.send("Failed to find anything.... Maybe refine the name..?")
               break
             else:
-             await text.channel.send("I failed to find any further results react with  1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ if you like any result.")
+             await message.channel.send("I failed to find any further results react with  1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ if you like any result.")
              break
-        elif text.content.startswith('/hello'):
-          if(text.author.name == 'druvvv'):
-            await text.channel.send("Hello Sir!!\U0001F6D0")
+        elif message.content.startswith('/hello'):
+          if(message.author.name == 'druvvv'):
+            await message.channel.send("Hello Sir!!\U0001F6D0")
           else:
-            await text.channel.send("Hello there!!")
-        elif text.content.startswith('/gpa'):
-          content = text.content.lstrip('/gpa ')
+            await message.channel.send("Hello there!!")
+        elif message.content.startswith('/gpa'):
+          content = message.content.lstrip('/gpa ')
           sgpa_list = content.split(' ')
-          gpa = (float(sgpa_list[0])*24 + float(sgpa_list[1])*25)/49.00
-          await text.channel.send('Your GPA is ' +'%.2f' % gpa)
+          gpa = (float(sgpa_list[0])*25 + float(sgpa_list[1])*24 + float(sgpa_list[2])*24)/73.00
+          await message.channel.send('Your GPA is ' +'%.2f' % gpa)
 
-        elif text.content.startswith('/youtube'):
-          content = text.content.lstrip('/youtube ')
+        elif message.content.startswith('/youtube'):
+          content = message.content.lstrip('/youtube ')
           youtubedownloader(content)
-          await text.channel.send(file = discord.File(f'./youtube.mp4'))
+          await message.channel.send(file = discord.File(f'./youtube.mp4'))
           os.remove(f'./youtube.mp4')
+
 
 
 
@@ -192,9 +192,9 @@ async def on_reaction_add(reaction , user):
     name = dictionary['name'][0]
     embedio = discord.Embed(
     title = name ,
-    desciption = "Embeded message" ,
-    colour = discord.Colour.blue())
-    embedio.set_author(name = user , icon_url = user.avatar_url)
+    description = "" ,
+    colour = discord.Colour.random())
+    embedio.set_author(name = user)
     embedio.add_field(name = 'Seeders' , value = dictionary['seeder'][0])
     size = int(dictionary['size'][0])/1073741824
     embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))
@@ -205,9 +205,9 @@ async def on_reaction_add(reaction , user):
     name = dictionary['name'][1]
     embedio = discord.Embed(
     title = name , 
-    desciption = "Embeded message" ,
-    colour = discord.Colour.blue())
-    embedio.set_author(name = user , icon_url = user.avatar_url)
+    description = "" ,
+    colour = discord.Colour.random())
+    embedio.set_author(name = user)
     embedio.add_field(name = 'Seeders' , value = dictionary['seeder'][1])
     size = int(dictionary['size'][1])/1073741824
     embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))
@@ -218,9 +218,9 @@ async def on_reaction_add(reaction , user):
     name = dictionary['name'][2]
     embedio = discord.Embed(
     title = name ,
-    desciption = "Embeded message" ,
-    colour = discord.Colour.blue())
-    embedio.set_author(name = user , icon_url = user.avatar_url)
+    description = "" ,
+    colour = discord.Colour.random())
+    embedio.set_author(name = user)
     embedio.add_field(name = 'Seeders' , value = dictionary['seeder'][2])
     size = int(dictionary['size'][2])/1073741824
     embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))
@@ -231,9 +231,9 @@ async def on_reaction_add(reaction , user):
     name = dictionary['name'][3]
     embedio = discord.Embed(
     title = name ,
-    desciption = "Embeded message" ,
-    colour = discord.Colour.blue())
-    embedio.set_author(name = user , icon_url = user.avatar_url)
+    description = "" ,
+    colour = discord.Colour.random())
+    embedio.set_author(name = user)
     embedio.add_field(name = 'Seeders' , value = dictionary['seeder'][3])
     size = int(dictionary['size'][3])/1073741824
     embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))
@@ -244,9 +244,9 @@ async def on_reaction_add(reaction , user):
     name = dictionary['name'][4]
     embedio = discord.Embed(
     title = name ,
-    desciption = "Embeded message" ,
-    colour = discord.Colour.blue())
-    embedio.set_author(name = user , icon_url = user.avatar_url)
+    description = "" ,
+    colour = discord.Colour.random())
+    embedio.set_author(name = user)
     embedio.add_field(name = 'Seeders' , value = dictionary['seeder'][4])
     size = int(dictionary['size'][4])/1073741824
     embedio.add_field(name = 'Size' , value = ('%.2f' % size+'GB'))
@@ -261,5 +261,4 @@ async def on_reaction_add(reaction , user):
 
 
 client.run(token)
-
 
